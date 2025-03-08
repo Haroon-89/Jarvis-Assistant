2c18f774d38f27d7737e344bfe490d9899a345c6
Haroon Iqbal <harooniqbal829@gmail.com>
2025-03-08 15:36:54 +0530

    Add files via upload

---
+++ musicLibrary.py
@@ -0,0 +1,5 @@
+music = {
+    "one": "https://www.youtube.com/watch?v=9sekgEXGm-E",
+    "faded": "https://www.youtube.com/watch?v=60ItHLz5WEA",
+    "way": "https://www.youtube.com/watch?v=dhYOPzcsbGM",
+}
